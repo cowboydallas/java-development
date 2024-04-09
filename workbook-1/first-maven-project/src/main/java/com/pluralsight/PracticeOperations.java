bd14002e4bc26c3b29a3305c53d4fe951d451fcf
Acton Barrett <abarrett@appdev.yearup.org>
2024-04-09 16:23:15 -0400

    exercise progress but not completed

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/PracticeOperations.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class PracticeOperations {
+    public static void main(String args[]) {
+        String word1 = "thing1";
+        String word2 = "thing2";
+        String word3 = "thing3";
+        String things;
+        things = word1 + " " + word2 + " " + word3;
+        System.out.println(things);
+    }
+}
--- workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -1,8 +1,12 @@
 package com.pluralsight;
-
+/**
+This was written by Acton Barrett
+This code was built to print a shopping
+ */
 public class ShoppingList {
 
     public static void main(String[] args) {
+        // This is my Shopping List
         System.out.println("Item 1");
         System.out.println("Item 2");
         System.out.println("Item 3");
